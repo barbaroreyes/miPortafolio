9ff3e1aee63368a711400fb41d20b43dd5f1a318
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-20 22:14:42 -0500

    added my picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './componet/header'
+import Header from './componet/header';
+import Pictures from './componet/im/pictures'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App tc">
        <Header/>
+       <Pictures/>
+
     </div>
   );
 }
---
+++ src/componet/im/pictures.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import work from '../img/work.jpg'
+import './img.css'
+
+function Pictures() {
+    return (
+        <div class= ' tc picture tc ma3 pa2 ' >
+           <img src={work} alt=''/>
+        </div>
+    )
+}
+
+export default Pictures
