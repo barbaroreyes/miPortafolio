f43d0efa35f7d61c5efad169b4e3a02e271ff632
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-20 21:30:18 -0500

    added a header

---
+++ src/componet/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+           <header className=' f3'>
+               <h1 className='dib '>Barbaro Reyes</h1>
+           </header>
+        </div>
+    )
+}
+
+export default Header
