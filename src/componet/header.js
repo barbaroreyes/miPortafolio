c3f09c57865b88f6f1c6d860689152fb528103c8
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-20 23:32:05 -0500

    added  some styles to links

--- src/componet/header.js
+++ src/componet/header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div>
            <header className=' f3'>
-               <h1 className='bg-light-green dib  ma3 pa2 shadow-5'>Barbaro Reyes</h1>
+               <h1 className='bg-light-green dib grow ma3 pa2 shadow-5'>Barbaro Reyes</h1>
            </header>
         </div>
     )
--- src/componet/nav/navigation.js
+++ src/componet/nav/navigation.js
@@ -4,11 +4,11 @@ import  './index.css'
 function Navigation() {
     return (
         <div className=' bg-light-green  tc'>
-           <button className='na'>Home</button>
-           <button className='na'>About Me</button>
-           <button className='na'><a href="https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/">LinKedln</a></button>
-           <button className='na'><a href="https://github.com/barbaroreyes">GitHub</a></button>
-           <button className='na'>Contact</button>
+           <button className='na grow'>Home</button>
+           <button className='na grow'>About Me</button>
+           <button className='na grow'><a href="https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/">LinKedln</a></button>
+           <button className='na grow'><a href="https://github.com/barbaroreyes">GitHub</a></button>
+           <button className='na grow'>Contact</button>
            
         </div>
     )
